adb22c62e4bab053e746051a8d0f55dcdcb4a0f9
Brett Labash <brett.labash@gmail.com>
2023-10-19 01:41:07 +0900

    Add IssuesList

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,70 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { GoIssueOpened, GoIssueClosed, GoComment } from "react-icons/go";
+import { relativeDate } from "../helpers/relativeDate";
+
+const IssueItem = ({
+  title,
+  number,
+  assignee,
+  comments,
+  createdBy,
+  createdDate,
+  labels,
+  status,
+}) => {
+  return (
+    <li>
+      <div>
+        {status === "done" || status === "cancelled" ? (
+          <GoIssueClosed style={{ color: "red" }} />
+        ) : (
+          <GoIssueOpened style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`issue/${number}`}>{title}</Link>
+          {labels.map((label) => (
+            <span key={label} className={`label red`}>
+              {label}
+            </span>
+          ))}
+        </span>
+        <small>
+          #{number} opened {relativeDate(createdDate)} by {createdBy}
+        </small>
+      </div>
+      {assignee && <div>{assignee}</div>}
+      <span className="comment-count">
+        {comments.length > 0 && (
+          <>
+            <GoComment />
+            {comments.length}
+          </>
+        )}
+      </span>
+    </li>
+  );
+};
 
 export default function IssuesList() {
+  const { data, isLoading, isError } = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
+  console.log("data", data);
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {data.map((issue) => (
+            <IssueItem key={issue.id} {...issue} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
