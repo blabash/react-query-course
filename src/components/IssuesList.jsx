2097629f8e1215d443f671c095334eb0d65fb027
Brett Labash <brett.labash@gmail.com>
2023-10-22 21:06:04 +0900

    Extract IssuesList and LabelList components

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,9 +1,16 @@
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 
-export default function IssuesList() {
-  const { data, isLoading, isError } = useQuery(["issues"], () =>
-    fetch("/api/issues").then((res) => res.json())
+export default function IssuesList({ labels, status }) {
+  const { data, isLoading, isError } = useQuery(
+    ["issues", { labels, status }],
+    () => {
+      const statusString = status ? `&status=${status}` : "";
+      const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+      return fetch(`/api/issues?${labelsString}${statusString}`).then((res) =>
+        res.json()
+      );
+    }
   );
 
   return (
--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,28 @@
-export default function LabelList() {
-  return <h3>Labels</h3>;
+import { useLabelsData } from "../helpers/useLabelsData";
+
+export default function LabelList({ selected, toggle }) {
+  const labelsQuery = useLabelsData();
+
+  return (
+    <div className="labels">
+      <h3>Labels</h3>
+      {labelsQuery.isLoading && <p>Loading labels...</p>}
+      {labelsQuery.isSuccess && (
+        <ul>
+          {labelsQuery.data.map((label) => (
+            <li key={label.id}>
+              <button
+                onClick={() => toggle(label.name)}
+                className={`label ${
+                  selected.includes(label.id) ? "selected " : " "
+                } ${label.color}`}
+              >
+                {label.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
